c9e99d25a59542774e6a1090fc5ac18944bba36a
Stevin Liang <liangzl@yutong.com>
2016-11-01 15:15:00 +0800

    Implement thread safe printf by using mutex to guard debug uart.
    Signed-off-by: Stevin Liang <liangzl@yutong.com>

--- system/board/src/retarget.c
+++ system/board/src/retarget.c
@@ -5,43 +5,63 @@
  *      to what you want
  */
 #include "board_api.h"
+#include "board.h"
+#include "FreeRTOS.h"
+#include "semphr.h"
+#include "retarget.h"
+
+static SemaphoreHandle_t xRetargetMutex = NULL;
+
 void retarget_init()
 {
-  // Initialize UART
+	// Initialize UART
+	xRetargetMutex = xSemaphoreCreateMutex();
+
+	if (xRetargetMutex != NULL)
+		xSemaphoreGive(xRetargetMutex);
 }
 
 int _write (int fd, char *ptr, int len)
 {
-  /* Write "len" of char from "ptr" to file id "fd"
-   * Return number of char written.
-   * Need implementing with UART here. */
-  int i;
+	/* Write "len" of char from "ptr" to file id "fd"
+	 * Return number of char written.
+	 * Need implementing with UART here. */
+#ifdef DEBUG_ENABLE
+	int i;
 
-  for ( i = 0; i < len; i++)
-    Board_UARTPutChar(ptr[i]);
-
-  return len;
+	if (xSemaphoreTake(xRetargetMutex, portMAX_DELAY) == pdTRUE) {
+		for ( i = 0; i < len; i++)
+			Board_UARTPutChar(ptr[i]);
+	}
+	xSemaphoreGive(xRetargetMutex);
+#endif
+	return len;
 }
 
 int _read (int fd, char *ptr, int len)
 {
-  /* Read "len" of char to "ptr" from file id "fd"
-   * Return number of char read.
-   * Need implementing with UART here. */
-  return len;
+	/* Read "len" of char to "ptr" from file id "fd"
+	 * Return number of char read.
+	 * Need implementing with UART here. */
+	return len;
 }
 
 void _ttywrch(int ch) {
-  /* Write one char "ch" to the default console
-   * Need implementing with UART here. */
-    Board_UARTPutChar((char) ch);
+	/* Write one char "ch" to the default console
+	 * Need implementing with UART here. */
+#ifdef DEBUG_ENABLE
+	if (xSemaphoreTake(xRetargetMutex, portMAX_DELAY) == pdTRUE)
+		Board_UARTPutChar((char) ch);
+
+	xSemaphoreGive(xRetargetMutex);
+#endif
 }
 
 /* SystemInit will be called before main */
 /*
 void SystemInit()
 {
-    retarget_init();
+	retarget_init();
 }
 */
 
