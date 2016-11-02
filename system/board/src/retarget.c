e67e30336cdf7138e55829cd3d728cfbb78edf2b
Stevin Liang <liangzl@yutong.com>
2016-11-02 16:07:13 +0800

    Using uart driver to retarget the printf
    Signed-off-by: Stevin Liang <liangzl@yutong.com>

--- system/board/src/retarget.c
+++ system/board/src/retarget.c
@@ -4,21 +4,13 @@
  *      This file is for retarget printf in newlib nano
  *      to what you want
  */
-#include "board_api.h"
-#include "board.h"
-#include "FreeRTOS.h"
-#include "semphr.h"
 #include "retarget.h"
-
-static SemaphoreHandle_t xRetargetMutex = NULL;
+#include "uart.h"
 
 void retarget_init()
 {
 	// Initialize UART
-	xRetargetMutex = xSemaphoreCreateMutex();
-
-	if (xRetargetMutex != NULL)
-		xSemaphoreGive(xRetargetMutex);
+	uart_init_all();
 }
 
 int _write (int fd, char *ptr, int len)
@@ -26,15 +18,8 @@ int _write (int fd, char *ptr, int len)
 	/* Write "len" of char from "ptr" to file id "fd"
 	 * Return number of char written.
 	 * Need implementing with UART here. */
-#ifdef DEBUG_ENABLE
-	int i;
+	uart_send(&uart0, (uint8_t *)ptr, len);
 
-	if (xSemaphoreTake(xRetargetMutex, portMAX_DELAY) == pdTRUE) {
-		for ( i = 0; i < len; i++)
-			Board_UARTPutChar(ptr[i]);
-	}
-	xSemaphoreGive(xRetargetMutex);
-#endif
 	return len;
 }
 
@@ -49,12 +34,7 @@ int _read (int fd, char *ptr, int len)
 void _ttywrch(int ch) {
 	/* Write one char "ch" to the default console
 	 * Need implementing with UART here. */
-#ifdef DEBUG_ENABLE
-	if (xSemaphoreTake(xRetargetMutex, portMAX_DELAY) == pdTRUE)
-		Board_UARTPutChar((char) ch);
-
-	xSemaphoreGive(xRetargetMutex);
-#endif
+	uart_send(&uart0, (uint8_t *)&ch, 1);
 }
 
 /* SystemInit will be called before main */
