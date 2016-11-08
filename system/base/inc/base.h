ae784f6955cf57ec045c1c9bbc7f6c6143ace918
Stevin Liang <liangzl@yutong.com>
2016-11-08 16:11:28 +0800

    Add base.h to export some useful MACROs and interfaces.
    Signed-off-by: Stevin Liang <liangzl@yutong.com>

---
+++ system/base/inc/base.h
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2016, ZHENGZHOU YUTONG BUS CO,.LTD.
+ * Author: Stevin Liang <liangzl@yutong.com>
+ * Date: Nov 8, 2016
+ *	base.h head file for exporting base macros and interfaces.
+ **/
+
+#ifndef _BASE_H
+#define _BASE_H
+/* get array size */
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
+
+#endif /* ifndef _BASE_H */
