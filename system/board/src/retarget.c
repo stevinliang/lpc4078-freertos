c3d13e84520261261eb346bd647746b6a20930e0
Stevin Liang <liangzl@yutong.com>
2016-11-11 17:51:52 +0800

    use posix interfaces to retarget printf to uart0
    Signed-off-by: Stevin Liang <liangzl@yutong.com>

--- system/board/src/retarget.c
+++ system/board/src/retarget.c
@@ -5,16 +5,11 @@
  *      to what you want
  */
 #include "retarget.h"
-#include "uart.h"
-
-#ifndef RETARGET_UART
-	#define RETARGET_UART (&uart0)
-#endif
+#include "posix.h"
 
 void retarget_init()
 {
 	// Initialize UART
-	uart_device_register(RETARGET_UART);
 }
 
 int _write (int fd, char *ptr, int len)
@@ -22,7 +17,7 @@ int _write (int fd, char *ptr, int len)
 	/* Write "len" of char from "ptr" to file id "fd"
 	 * Return number of char written.
 	 * Need implementing with UART here. */
-	return uart_send(RETARGET_UART, (uint8_t *)ptr, len);
+	return write(fd, (const void *)ptr, len);
 }
 
 int _read (int fd, char *ptr, int len)
@@ -30,13 +25,13 @@ int _read (int fd, char *ptr, int len)
 	/* Read "len" of char to "ptr" from file id "fd"
 	 * Return number of char read.
 	 * Need implementing with UART here. */
-	return uart_recv(RETARGET_UART, (uint8_t *)ptr, len);
+	return read(fd, (void *)ptr, len);
 }
 
 void _ttywrch(int ch) {
 	/* Write one char "ch" to the default console
 	 * Need implementing with UART here. */
-	uart_send(RETARGET_UART, (uint8_t *)&ch, 1);
+	write(1, (const void*)&ch, 1);
 }
 
 /* SystemInit will be called before main */
